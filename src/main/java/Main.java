6318408246fd92b6a1e63af1d6e548d066e02785
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-09-05 19:54:09 +0300

    made a discord bot -initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,47 @@
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+import static net.dv8tion.jda.api.AccountType.*;
+
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = JDABuilder.createDefault("ODg0MDQxMDU4MjkwMzk3MTk0.YTStZA.aaRvWOZCGGnTEP3Ay_gXX1Lmzg0");
+        builder.addEventListeners(new Main());
+        builder.build();
+    }
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+        System.out.println("We received a new message from "
+                + event.getAuthor().getName() + ": "
+                + event.getMessage().getContentDisplay());
+
+        if(event.getMessage().getContentRaw().equals("naber"))
+        {
+            event.getChannel().sendMessage("iyi ya nolsun iste bot yapiyoz senden naber :D").queue();
+        }
+        if(event.getMessage().getContentRaw().equals("aferim bana"))
+        {
+            event.getChannel().sendMessage("yess aferim lan bana hehe").queue();
+        }
+        if(event.getMessage().getContentRaw().equals("harikayim lan ben"))
+        {
+            event.getChannel().sendMessage("BEN MUHTESEMIMM !! ").queue();
+        }
+        if(event.getMessage().getContentRaw().equals("hehehe"))
+        {
+            event.getChannel().sendMessage("NIHUHAHAHAAHAAAAA!!!! :D :D").queue();
+        }
+        if(event.getMessage().getContentRaw().equals("BOT BANA CEVAP VERR"))
+        {
+            event.getChannel().sendMessage("tamam abi hemen abi").queue();
+        }
+        if ((event.getMessage().getContentRaw().contains("naber") || event.getMessage().getContentRaw().contains("meraba")) || event.getMessage().getContentRaw().contains("merhaba"))
+        {
+            event.getChannel().sendMessage("Selamm, meraba bende bot ;) ayakkabı olan değil hahahahaaa").queue();
+        }
+    }
+}
